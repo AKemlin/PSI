7dbb87a3a116cbc2e1bd8d13ba9a3b449ea276c5
Paul REYMOND <paul.reymond@edu.devinci.fr>
2024-03-06 16:22:38 +0100

    test2

--- PSI/PSI/Program.cs
+++ PSI/PSI/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+static void 
+Bitmap b = new Bitmap("./Images/lena.bmp");
